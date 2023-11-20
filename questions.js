8f86518308b0e073123e193d57e932078f33e74c
Michael <michael.riordan81@outlook.com>
2023-11-20 15:54:43 -0700

    add new solutions

--- questions.js
+++ questions.js
@@ -213,6 +213,103 @@ console.log(tournamentWinner(competitions, results));
 
 /*----------------------------------------------------------------------------------------------------*/
 //NON-CONSTRUCTIBLE CHANGE;
+/*
+const coins = [1, 1, 1, 1, 1];
+
+function nonConstructibleChange(coins) {
+    const sortedCoins = coins.sort((a, b) => a - b);
+    let minimumPossibleChange = 0;
+
+    for (const value of sortedCoins) {
+        if (value > minimumPossibleChange + 1) {
+            break;
+        } else if (value <= minimumPossibleChange + 1) {
+            minimumPossibleChange += value;
+        }
+    }
+
+    return minimumPossibleChange + 1;
+}
+
+console.log(nonConstructibleChange(coins));
+*/
+
+/*----------------------------------------------------------------------------------------------------*/
+//TRANSPOSE MATRIX;
+//time and space complexity = O(n * m);
+/*
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+function transposeMatrix(matrix) {
+    let transposedMatrix = new Array(matrix[0].length);
+    for (let i = 0; i < matrix[0].length; i++) {
+        transposedMatrix[i] = new Array(matrix.length);
+    }
+
+    for (let idx = 0; idx < matrix.length; idx++) {
+        for (let i = 0; i < matrix[idx].length; i++) {
+            transposedMatrix[i][idx] = matrix[idx][i];
+        }
+    }
+
+    return transposedMatrix;
+}
+
+console.log(transposeMatrix(matrix));
+*/
+
+/*----------------------------------------------------------------------------------------------------*/
+//FIND CLOSEST VALUE IN BST (BINARY SEARCH TREE);
+const tree = {
+    "nodes": [
+      {"id": "10", "left": "5", "right": "15", "value": 10},
+      {"id": "15", "left": "13", "right": "22", "value": 15},
+      {"id": "22", "left": null, "right": null, "value": 22},
+      {"id": "13", "left": null, "right": "14", "value": 13},
+      {"id": "14", "left": null, "right": null, "value": 14},
+      {"id": "5", "left": "2", "right": "5-2", "value": 5},
+      {"id": "5-2", "left": null, "right": null, "value": 5},
+      {"id": "2", "left": "1", "right": null, "value": 2},
+      {"id": "1", "left": null, "right": null, "value": 1}
+    ],
+    "root": "10"
+  }
+const target = 12;
+
+function findClosestValueInBst(tree, target) {
+    return findClosestValueInBstHelper(tree, target, tree.value);
+}
+
+function findClosestValueInBstHelper(tree, target, closest) {
+    if (tree === null) return closest;
+    if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+        closest = tree.value;
+    }
+
+    if (target < tree.value) {
+        return findClosestValueInBstHelper(tree.left, target, closest);
+    } else if (target > tree.value) {
+        return findClosestValueInBstHelper(tree.right, target, closest);
+    } else {
+        return closest;
+    }
+}
+
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const closestValue = findClosestValueInBst(tree, target);
+console.log(closestValue);
+
 
 
 
